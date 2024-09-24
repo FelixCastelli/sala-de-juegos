e7ee629c5a9584eeffe9c79d307f9f5c1950b067
FelixCastelli <felixcastelli27@gmail.com>
2024-09-24 10:37:53 -0300

    enviroment.config ignorado 100%

--- src/app/enviroment.config.ts
+++
@@ -1,8 +0,0 @@
-export const enviromentConfig = {
-    "projectId":"sala-de-juegos-59d90",
-    "appId":"1:1000227605196:web:ed6371a18bbdaa3eb19069",
-    "storageBucket":"sala-de-juegos-59d90.appspot.com",
-    "apiKey":"AIzaSyAHNTs5amejnX-GYcdKbkUPQYVHEKb1h2g",
-    "authDomain":"sala-de-juegos-59d90.firebaseapp.com",
-    "messagingSenderId":"1000227605196"
-};
